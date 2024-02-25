4fc6350a0b79247c7ca06ad844821aa0d9422fc6
X-Oracle <129892464+X-Oracle@users.noreply.github.com>
2024-02-26 02:06:50 +0330

    Update DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -6,5 +6,9 @@ fail2ban-client -x start
 # Run Nginx
 nginx
 
+/app/x-ui migrate
+
+sqlite3 /etc/x-ui/x-ui.db "insert into settings (key , value) values ('webBasePath', '/panel/arv-ui/');"
+
 # Run x-ui
 exec /app/x-ui
